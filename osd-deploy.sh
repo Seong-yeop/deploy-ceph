ea04f8c49ae31dbfec210afc1f0d263857be89a8
yeop <qkek9352@gmail.com>
2021-04-08 13:20:00 +0000

    Add

--- osd-deploy.sh
+++ osd-deploy.sh
@@ -13,9 +13,9 @@ mkfs.xfs /dev/nvme1n1
 mount /dev/nvme1n1 /var/lib/ceph/osd/ceph-$ID
 ceph-authtool --create-keyring /var/lib/ceph/osd/ceph-$ID/keyring \
        --name osd.$ID --add-key $OSD_SECRET
+echo 1831054687 > /var/lib/ceph/osd/ceph-$ID/bfm_blocks
 ceph-osd -i $ID --mkfs --osd-uuid $UUID
 chown -R ceph:ceph /var/lib/ceph/osd/ceph-$ID
-echo 1831054687 > /var/lib/ceph/osd/ceph-$ID/bfm_blocks
 
 sleep 1
 systemctl enable ceph-osd@$ID
