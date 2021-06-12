e57557b082fc90912fc62f40fd36e41208813b7c
yeop <qkek9352@gmail.com>
2021-06-12 10:16:34 +0000

    Add

--- rgw-deploy.sh
+++ rgw-deploy.sh
@@ -5,15 +5,24 @@ rgw_hostname=`hostname -s`
 systemctl stop ceph.target
 
 rm -rf /var/lib/ceph/radosgw/*
-mkdir -p /var/lib/ceph/radosgw/ceph-rgw.$rgw_hostname
-ceph auth get-or-create client.rgw.$rgw_hostname osd 'allow rwx' mon 'allow rw' -o /var/lib/ceph/radosgw/ceph-rgw.$rgw_hostname/keyring
-touch /var/lib/ceph/radosgw/ceph-rgw.$rgw_hostname/done
 
-chown -R ceph:ceph /var/lib/ceph/radosgw
-chown -R ceph:ceph /var/log/ceph
-chown -R ceph:ceph /var/run/ceph
-chown -R ceph:ceph /etc/ceph
+for i in {1..4}; do
+  rgw="${rgw_hostname}-${i}"
+  echo "${rgw} starting" 
+  mkdir -p /var/lib/ceph/radosgw/ceph-rgw.$rgw
+  ceph auth get-or-create client.rgw.$rgw osd 'allow rwx' mon 'allow rw' -o /var/lib/ceph/radosgw/ceph-rgw.$rgw/keyring
+  touch /var/lib/ceph/radosgw/ceph-rgw.$rgw/done
 
-systemctl enable ceph-radosgw.target
-systemctl enable ceph-radosgw@rgw.$rgw_hostname
-systemctl start ceph-radosgw@rgw.$rgw_hostname
+  chown -R ceph:ceph /var/lib/ceph/radosgw
+  chown -R ceph:ceph /var/log/ceph
+  chown -R ceph:ceph /var/run/ceph
+  chown -R ceph:ceph /etc/ceph
+	
+  if [ ${i} -eq 1 ];then
+    systemctl enable ceph-radosgw.target
+  fi
+  systemctl enable ceph-radosgw@rgw.$rgw
+  systemctl start ceph-radosgw@rgw.$rgw
+
+done 
+
